42df47c6eeb71621df65e701bc63876d3238e2ce
pratik-sahu-gb <pratik.sahu@gobaskt.com>
2022-06-27 13:12:03 +0530

    update

--- Assignments/module-4/script.js
+++ Assignments/module-4/script.js
@@ -30,7 +30,7 @@ WARNING!!! WARNING!!!
 
 (function () {
 
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim","Pratik"];
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 for (var i = 0; i < names.length; i++) {
   var firstLetter = names[i].charAt(0).toLowerCase();
